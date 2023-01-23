9c982ae027b7252656b4067690119b62362c7af2
williamjones <willj836@gmail.com>
2023-01-23 13:13:33 +0000

    pulled date validation out into priv method

--- bankAccount.js
+++ bankAccount.js
@@ -9,12 +9,20 @@ class BankAccount {
     if (!Number.isInteger(amount) || amount < 1) {
       throw new Error("Invalid amount - amount must be a positive integer");
     }
+    this.#validateDate(date);
+    this.balance += amount;
+    this.transactions.push({ date, amount, type: "deposit" });
+  }
+
+  withdraw(amount, date) {
+    this.#validateDate(date);
+  }
+
+  #validateDate(date) {
     const dateObject = new Date(date);
     if (dateObject == "Invalid Date") {
       throw new Error("Invalid Date - use format YYYY-MM-DD");
     }
-    this.balance += amount;
-    this.transactions.push({ date, amount, type: "deposit" });
   }
 }
 
