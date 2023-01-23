d5e5014d8eb4297eac44a102b0da52d730995d5a
williamjones <willj836@gmail.com>
2023-01-23 12:50:10 +0000

    deposit throws error if amount is a float

--- bankAccount.test.js
+++ bankAccount.test.js
@@ -44,6 +44,11 @@ describe("Bank Account", () => {
           "Invalid amount - amount must be a positive integer"
         );
       });
+      it("should throw an error if amount is not an integer (A Float)", () => {
+        expect(() => bankAccount.deposit(100.5, "2020-01-01")).toThrowError(
+          "Invalid amount - amount must be a positive integer"
+        );
+      });
     });
   });
 });
