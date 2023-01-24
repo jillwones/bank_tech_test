c696987fa8ac9b7bcaec26283141516e077b7eb7
williamjones <willj836@gmail.com>
2023-01-24 11:32:25 +0000

    print method tested and built

--- bankStatement.js
+++ bankStatement.js
@@ -1,10 +1,28 @@
 class BankStatement {
+  print(transactions) {
+    const formattedTransactions = transactions
+      .map((transaction) => {
+        return this.formatTransaction(transaction);
+      })
+      .reverse();
+    console.table(formattedTransactions);
+    return formattedTransactions;
+  }
+
+  formatTransaction(transaction) {
+    transaction.date = this.formatDate(transaction.date);
+    transaction.credit = this.formatAmount(transaction.credit);
+    transaction.debit = this.formatAmount(transaction.debit);
+    transaction.balance = this.formatAmount(transaction.balance);
+    return transaction;
+  }
+
   formatDate(date) {
     return date.toLocaleDateString("en-GB");
   }
 
   formatAmount(amount) {
-    return amount.toFixed(2);
+    return amount && amount.toFixed(2);
   }
 }
 
--- bankStatement.test.js
+++ bankStatement.test.js
@@ -5,14 +5,82 @@ describe("Bank Statement", () => {
   beforeEach(() => {
     bankStatement = new BankStatement();
   });
-  describe("formatting extras", () => {
+  describe("formatDate", () => {
     it("correctly formats date", () => {
       const date = new Date("2023-01-24T10:59:31.832Z");
       expect(bankStatement.formatDate(date)).toEqual("24/01/2023");
     });
-
+  });
+  describe("formatAmount", () => {
     it("correctly formats amount when passed amount", () => {
       expect(bankStatement.formatAmount(200)).toEqual("200.00");
+    });
+  });
+  describe("formatTransaction", () => {
+    it("correctly formats the transaction", () => {
+      const transaction = {
+        date: new Date("2023-01-24T10:59:31.832Z"),
+        credit: 10,
+        debit: null,
+        balance: 10,
+      };
+      const expectedFormattedTransaction = {
+        date: "24/01/2023",
+        credit: "10.00",
+        debit: null,
+        balance: "10.00",
+      };
+      expect(bankStatement.formatTransaction(transaction)).toEqual(
+        expectedFormattedTransaction
+      );
+    });
+  });
+  describe("print", () => {
+    it("prints the transactions in reverse order", () => {
+      const transactions = [
+        {
+          date: new Date("2023-01-24"),
+          credit: 10,
+          debit: null,
+          balance: 10,
+        },
+        {
+          date: new Date("2023-01-26"),
+          credit: 50,
+          debit: null,
+          balance: 60,
+        },
+        {
+          date: new Date("2023-01-29"),
+          credit: null,
+          debit: 40,
+          balance: 20,
+        },
+      ];
+
+      const expectedFormattedTransaction = [
+        {
+          date: "29/01/2023",
+          credit: null,
+          debit: "40.00",
+          balance: "20.00",
+        },
+        {
+          date: "26/01/2023",
+          credit: "50.00",
+          debit: null,
+          balance: "60.00",
+        },
+        {
+          date: "24/01/2023",
+          credit: "10.00",
+          debit: null,
+          balance: "10.00",
+        },
+      ];
+      expect(bankStatement.print(transactions)).toEqual(
+        expectedFormattedTransaction
+      );
     });
   });
 });
